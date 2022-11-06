13675f3fa1fc15a3ca9d615f7a45cd7386771430
swagoverlord <azcooldude8@gmail.com>
2022-11-06 22:57:10 +0100

    Created custom responsive breakpoints

--- vaservah/tailwind.config.js
+++ vaservah/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens: {
+      phone: "480px",
+      // => @media (min-width: 480px) { ... }
+
+      tablet: "760px",
+      // => @media (min-width: 760px) { ... }
+
+      desktop: "1000px",
+      // => @media (min-width: 1000px) { ... }
+    },
   },
   plugins: [],
-}
+};
