137164e55afba2c93ea1168e06619d49792e1b3e
swagoverlord <azcooldude8@gmail.com>
2022-11-06 21:20:52 +0100

    Update app.module.ts

--- vaservah/src/app/app.module.ts
+++ vaservah/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PostComponent } from './components/post/post.component';
+import { ErrorComponent } from './pages/error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    PostsComponent,
+    ContactComponent,
+    PostComponent,
+    ErrorComponent,
+    
   ],
   imports: [
     BrowserModule,
