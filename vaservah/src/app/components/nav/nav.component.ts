f33d7eff5d1af1a66ed8cac5b76536fefd6fe875
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-11-10 03:54:58 +0100

    Main nav loads urls from urls.json

--- vaservah/src/app/components/nav/nav.component.ts
+++ vaservah/src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from 'src/app/interfaces/Link';
+import { GetUrlsService } from 'src/app/services/get-urls.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private urls: GetUrlsService) { }
+
+  links: Link[] = [];
 
   ngOnInit(): void {
+    this.urls.getUrls().subscribe((data) => this.links = data);
   }
 
 }
