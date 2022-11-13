f3e3c2eb2f35af1817300240acdb304ce6e205e9
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-11-13 07:45:35 +0100

    Temporary way to load posts
    Need to find a way to put the posts in the posts: Post[] variable

--- vaservah/src/app/pages/posts/posts.component.ts
+++ vaservah/src/app/pages/posts/posts.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { GetPostsService } from 'src/app/services/get-posts.service';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/interfaces/Post';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  tmpPosts: any[] = [];
+  posts: Post[] = [];
+
+  constructor(private postsData: GetPostsService) { }
 
   ngOnInit(): void {
+    this.postsData.getPosts(15).subscribe((data) => {
+      //this.posts = data;
+      this.tmpPosts = data;
+    });
   }
-
 }
