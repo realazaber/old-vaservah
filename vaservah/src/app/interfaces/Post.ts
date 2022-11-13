f22fa20e110efd8193277866a79a6e760caff3b6
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-11-13 19:01:09 +0100

    Update Post.ts

--- vaservah/src/app/interfaces/Post.ts
+++ vaservah/src/app/interfaces/Post.ts
@@ -3,24 +3,28 @@ export class Post {
     title: string;
     body: string;
     excerpt: string;
+    category: string;
     teaserPath: string;
 
     postPath: string;
     wpPostPath: string;
-    category: string;
-
+    
     constructor(id: number, title: string, body: string, teaserPath: string, postPath: string, wpPostPath: string) {
         this.id = id;
         this.title = title;
         this.body = body;
+
+        // Set excerpt as substring of body.
         this.excerpt = body.substring(0, 50);
+        
         this.teaserPath = teaserPath;
         this.postPath = postPath;
         this.wpPostPath = wpPostPath;
 
+        // Set category from the Wordpress post path.
         const splitWpPath: string[] = this.wpPostPath.split("/");
         this.category = splitWpPath[splitWpPath.length - 3];
 
-        console.log(this.wpPostPath.split("/"));
+        
     }
 }
