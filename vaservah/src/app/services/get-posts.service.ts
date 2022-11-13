5a61c2eb85e0d60b80e593fee5571e05220c5de9
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-11-13 03:00:28 +0100

    Base post interface and service

--- vaservah/src/app/services/get-posts.service.ts
+++ vaservah/src/app/services/get-posts.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Post } from '../interfaces/Post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetPostsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  posts: Post[] = [];
+  post: Post = {};
+
+  getPosts(): Post[] {
+    return this.posts;
+  }
+
+  getPost(postId: Number): Post {
+    return this.post;
+  }
 }
