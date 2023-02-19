9b80fe10551fcaf67caec601dfb9b4b9d85d14f7
swagoverlord <azcooldude8@gmail.com>
2023-02-19 22:00:03 +0100

    remove scully

--- vaservah/scully.vaservah.config.ts
+++
@@ -1,9 +0,0 @@
-import { ScullyConfig } from '@scullyio/scully';
-import '@scullyio/scully-plugin-puppeteer';
-
-export const config: ScullyConfig = {
-  projectRoot: './src/',
-  projectName: 'vaservah',
-  outDir: './dist/static',
-  routes: {},
-};
